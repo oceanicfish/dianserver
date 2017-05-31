83ed8e6260dbbdde824fbfef82ded705f80c6370
yang <everymoment.studio@gmail.com>
2017-05-31 14:29:14 +0800

    added migration photos

--- database/migrations/2017_05_31_062835_create_photos_table.php
+++ database/migrations/2017_05_31_062835_create_photos_table.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateGalleryTable extends Migration
+class CreatePhotosTable extends Migration
 {
     /**
      * Run the migrations.
@@ -12,13 +12,9 @@ class CreateGalleryTable extends Migration
      */
     public function up()
     {
-        Schema::create('gallery', function (Blueprint $table) {
+        Schema::create('photos', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('path');
-            $table->string('caption');
-            $table->string('tags');
             $table->timestamps();
-
         });
     }
 
@@ -29,6 +25,6 @@ class CreateGalleryTable extends Migration
      */
     public function down()
     {
-        Schema::drop('gallery');
+        Schema::drop('photos');
     }
 }
