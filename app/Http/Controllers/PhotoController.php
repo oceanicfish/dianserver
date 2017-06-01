1057d7478a15ef0fa3574045534130d2cff5fcb2
yang <everymoment.studio@gmail.com>
2017-06-01 13:55:59 +0800

    added photos upload and insert into DB

--- app/Http/Controllers/PhotoController.php
+++ app/Http/Controllers/PhotoController.php
@@ -17,9 +17,9 @@ class PhotoController extends Controller
     public function __construct()
     {
 //        $this->middleware("auth");
-        echo "TOKEN: <br>";
-        echo csrf_token();
-        echo "<br>";
+//        echo "TOKEN: <br>";
+//        echo csrf_token();
+//        echo "<br>";
     }
 
     /**
@@ -38,6 +38,12 @@ class PhotoController extends Controller
     public function upload(Request $request)
     {
 
+        $caption = $request->input('caption');
+        $tags = $request->input('tags');
+        $watermark = Image::make(Input::file('watermark'));
+        $position = $request->input('position');
+        $size = (int)$request->input('size');
+        $url_prefix = 'http://server.diandianplay.cn/';
 
         $photos = Input::file('photos');
 
@@ -47,14 +53,22 @@ class PhotoController extends Controller
 
             $image = Image::make($photo);
 
-            $image->rotate(180);
-            $image->insert('uploads/qrcode.jpg', 'bottom-right', 10, 10);
+//            $image->rotate(180);
+//            $image->insert('uploads/qrcode.jpg', 'bottom-right', 10, 10);
+            $image->insert($watermark, $position, $size, $size);
             $file_name = 'uploads/'. strval(time()) . strval($i) . '.jpg';
             $image->save($file_name);
+
+            Photo::create([
+                'path' => $url_prefix . $file_name,
+                'caption' => $caption,
+                'tags' => $tags
+            ]);
+
             $i++;
         }
 
-        echo "done";
+        return back();
 
     }
 
