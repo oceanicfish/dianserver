7320a60e4b3ff10cf80c5751269f3d147c9c8bd5
yang <everymoment.studio@gmail.com>
2017-06-05 21:48:36 +0800

    debug user 4

--- app/Http/Controllers/WechatController.php
+++ app/Http/Controllers/WechatController.php
@@ -15,7 +15,7 @@ class WechatController extends Controller
      * @param Request $request
      * @return mixed
      */
-    public function serve(Request $request, $wechatServer)
+    public function serve(Request $request)
     {
         /**
          * EasyWeChat = $app
