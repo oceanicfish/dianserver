6ddab964f15c5acab706c5bdf6d99c7714bc145b
yang <everymoment.studio@gmail.com>
2017-06-05 13:56:32 +0800

    fixed cros Call to undefined method Symfony\Component\HttpFoundation\Response::header()

--- app/Http/Middleware/Cors.php
+++ app/Http/Middleware/Cors.php
@@ -15,8 +15,10 @@ class Cors
      */
     public function handle($request, Closure $next)
     {
-        return $next($request)
-            ->header('Access-Control-Allow-Origin', '*')
-            ->header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+        $response = $next($request);
+        $response->headers->set('Access-Control-Allow-Origin', '*');
+        $response->headers->set('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE, OPTIONS');
+        $response->headers->set('Access-Control-Allow-Headers', 'Content-Type, Authorization, X-XSRF-TOKEN');
+        return $response;
     }
 }
