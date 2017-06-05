7c93ebe00775b3e3d5d675a156a775e0ce48c911
yang <everymoment.studio@gmail.com>
2017-06-05 14:18:11 +0800

    added receive message from wechat

--- app/Http/Controllers/WechatController.php
+++ app/Http/Controllers/WechatController.php
@@ -19,7 +19,38 @@ class WechatController extends Controller
          * EasyWeChat = $app
          * EasyWeChat::server() = $app->server()
          */
-        $wechatServer = EasyWeChat::server();
-        return $wechatServer->serve();
+        $wechat = EasyWeChat::server();
+//        return $wechat->serve();
+
+        $wechat->setMessageHandler(function ($message) {
+            switch ($message->MsgType) {
+                case 'event':
+                    return '收到事件消息';
+                    break;
+                case 'text':
+                    return '收到文字消息';
+                    break;
+                case 'image':
+                    return '收到图片消息';
+                    break;
+                case 'voice':
+                    return '收到语音消息';
+                    break;
+                case 'video':
+                    return '收到视频消息';
+                    break;
+                case 'location':
+                    return '收到坐标消息';
+                    break;
+                case 'link':
+                    return '收到链接消息';
+                    break;
+                // ... 其它消息
+                default:
+                    return '收到其它消息';
+                    break;
+            }
+            // ...
+        });
     }
 }
