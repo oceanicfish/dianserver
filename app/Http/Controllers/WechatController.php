67cc2a1b857a835e2a54d8d637b3412eaf59a3c8
yang <everymoment.studio@gmail.com>
2017-07-04 15:38:51 +0800

    temprarily removed event message handling

--- app/Http/Controllers/WechatController.php
+++ app/Http/Controllers/WechatController.php
@@ -112,17 +112,17 @@ class WechatController extends Controller
         $user = $this->userService->get($openID);
         // $message->MsgType // 消息类型：event, text....
         $returnMsg =", 欢迎关注测试号!";
-        if ($message->MsgType == 'event') {
-            Log::DEBUG("&&&& message event key : " . $message->EventKey);
-            switch ($message->EventKey) {
-                case 'V1001_GOOD':
-                    $returnMsg = ", 谢谢亲";
-                    break;
-                default:
-                    $returnMsg = "";
-                    break;
-            }
-        }
+//        if ($message->MsgType == 'event') {
+//            Log::DEBUG("&&&& message event key : " . $message->EventKey);
+//            switch ($message->EventKey) {
+//                case 'V1001_GOOD':
+//                    $returnMsg = ", 谢谢亲";
+//                    break;
+//                default:
+//                    $returnMsg = "";
+//                    break;
+//            }
+//        }
 
         return "您好！".$user->nickname.$returnMsg;
     }
