14d1896bc26676345ead8d9ac4788fd91a7ee898
yang <everymoment.studio@gmail.com>
2017-07-06 17:27:25 +0800

    fixed added message handling in order function

--- app/Http/Controllers/WechatController.php
+++ app/Http/Controllers/WechatController.php
@@ -23,7 +23,7 @@ class WechatController extends Controller
     /**
      * @var wechat openid
      */
-    public static $openID = "";
+    public $openID = null;
 
     /**
      * WechatController constructor.
@@ -146,6 +146,14 @@ class WechatController extends Controller
      */
     public function order($sid)
     {
+
+        $this->wechatService->setMessageHandler(function ($message) {
+            // $message->FromUserName // 用户的 openid
+            // $message->MsgType // 消息类型：event, text....
+            $this->openID = $message->FromUserName;
+        });
+
+
         Log::INFO("enter order function, showid=". $sid . ", openid=" . $this->openID);
 
         $payment = EasyWeChat::payment();
