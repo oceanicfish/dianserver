417cfffda7b888c70d07a163c39b332e8d77d5ad
Jessie Xia <everymoment.studio@gmail.com>
2017-11-21 12:04:25 +0800

    added  wechat x5 browser compatiable meta tags !!! important !!!

--- public/ticket/panda.php
+++ public/ticket/panda.php
@@ -3,6 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <title>点点儿童剧团</title>
+    <!--    very important for wechat X5 browser -->
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="Nova theme" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css" integrity="sha384-PsH8R72JQ3SOdhVi3uxftmaW6Vc51MKb0q5P2rRUpPvrszuE4W1povHYgTpBfshb" crossorigin="anonymous">
